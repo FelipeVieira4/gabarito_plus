8c680cb5e3c76b523f69e7b74250448d804d8148
XD-Enrique <enriquezozdesouza@gmail.com>
2026-08-23 19:49:38 -0300

    feat: adicionar modelo de domínio e mock do professor

---
+++ lib/models/professor.dart
@@ -0,0 +1,13 @@
+class Professor {
+  final String id;
+  final String nome;
+  final String email;
+  final String departamento;
+
+  Professor({
+    required this.id,
+    required this.nome,
+    required this.email,
+    required this.departamento,
+  });
+}
